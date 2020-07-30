4242d58554227d0283e13b3e6fdf3e3400f43deb
franhermani <franhermani@gmail.com>
2020-07-30 19:51:55 -0300

    New attribute

--- caja_ahorro_dolares.d
+++ caja_ahorro_dolares.d
@@ -3,14 +3,18 @@ import cuenta;
 import imponible;
 
 class CajaAhorroDolares : Cuenta, Imponible {
+private:
+	float constanteMensual;
+
 public:
 	this(int nro_cuenta, float limite_extraccion) {
 		super(nro_cuenta, limite_extraccion);
 		tipo_moneda.moneda = "DOLAR";
 		tipo_moneda.simbolo = "USD";
+		constanteMensual = 0.1;
 	}
 
 	float calcularCostoMensual() {
-		return 0.1 * limiteExtraccion;
+		return constanteMensual * limiteExtraccion;
 	}
 }
